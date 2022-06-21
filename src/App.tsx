7454e6c4d7ec8aa5b86f9964622d64543be48cd0
071yoon <yeonggilol@gmail.com>
2022-06-21 20:49:47 +0900

    FIRE-129 add dependency
    styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import styled from 'styled-components';
+
 export default function App() {
   return (
     <div>
-      <p>HI</p>
+      <Container>HI</Container>
     </div>
   );
 }
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: lightskyblue;
+`;
