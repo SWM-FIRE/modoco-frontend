d82920ea6f7b580349005af0d91f522e8dcd3f92
071yoon <yeonggilol@gmail.com>
2022-06-21 21:03:11 +0900

    FIRE-129 add dependency
    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
 
-export default function App() {
+function Router() {
   return (
-    <div>
-      <Container>HI</Container>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TestContainer />} />
+        <Route path="/test2" element={<TestContainer2 />} />
+        <Route path="/test3" element={<TestContainer3 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-const Container = styled.div`
+const TestContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: lightskyblue;
+  background-color: lightcyan;
 `;
+const TestContainer2 = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: lightgreen;
+`;
+
+const TestContainer3 = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: lightsalmon;
+`;
+
+export default Router;
