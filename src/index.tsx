69058a778881223af9c890a1eca48ca9039f6f6a
071yoon <yeonggilol@gmail.com>
2022-06-22 14:08:49 +0900

    FIRE-130 rename folder
    rename Styles to styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import GlobalStyle from './Styles/GlobalStyle';
+import GlobalStyle from './styles/GlobalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
