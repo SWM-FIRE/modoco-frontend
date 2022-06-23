83cbe1bcc536eb6b2fb2b27d3350170b8148403a
071yoon <yeonggilol@gmail.com>
2022-06-23 16:16:18 +0900

    FIRE-133 edit index
    react18에 맞춰 root에서 render 할 수 있도록 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import GlobalStyle from './styles/global';
 import { defaultTheme } from './styles/theme';
 
-ReactDOM.render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = createRoot(rootElement);
+root.render(
+  <StrictMode>
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </StrictMode>,
 );
