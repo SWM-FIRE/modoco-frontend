e111dd98546636e8d5e404e99c9aa8319ba4c4bd
YeongGi Yoon <yeonggi@kakao.com>
2022-06-29 13:51:51 +0900

    add room api

--- src/components/lobby/Room.tsx
+++ src/components/lobby/Room.tsx
@@ -1,11 +1,30 @@
-import roomAPI from '../../rooms.json';
+import { useEffect, useState } from 'react';
 import RoomBlock from './RoomBlock';
 import roomInterface from '../../room.interface';
 
 export default function Room() {
+  const [rooms, setRooms] = useState([
+    {
+      id: 'init',
+      name: 'initName',
+      total: 5,
+      current: 3,
+    },
+  ]);
+  useEffect(() => {
+    const API_URL: string = process.env.REACT_APP_SERVER as string;
+    console.log(API_URL);
+    fetch(API_URL, {
+      headers: {
+        accept: 'application/json',
+      },
+      method: 'GET',
+    }).then((res) => res.json().then((data) => setRooms(data)));
+  }, []);
+
   return (
     <>
-      {roomAPI.map(({ name, total, current, id }: roomInterface) => {
+      {rooms.map(({ id, name, total, current }: roomInterface) => {
         return (
           <RoomBlock
             key={id}
--- src/room.interface.ts
+++ src/room.interface.ts
@@ -1,6 +1,6 @@
 export default interface roomInterface {
+  id: string;
   name: string;
   total: number;
   current: number;
-  id: string;
 }
