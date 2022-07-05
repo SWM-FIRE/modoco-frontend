62c103b4e4927b656934e4ac4a265b2e2a197cc3
YeongGi Yoon <yeonggi@kakao.com>
2022-07-05 16:03:10 +0900

    FIRE-208 switch to displaymedia
    원래 webcam을 활용한 공유에서 display를 공유하는 형태로 변경

--- src/components/rtc/hooks/useCreateMediaStream.ts
+++ src/components/rtc/hooks/useCreateMediaStream.ts
@@ -5,15 +5,14 @@ export const useCreateMediaStream = (
   localVideoRef: RefObject<HTMLVideoElement>,
 ) => {
   const [userMediaStream, setUserMediaStream] = useState<MediaStream>();
-
+  /**
+   * @stream getUserMedia - webCam
+   * @stream getDisplayMedia - screen
+   */
   useEffect(() => {
     const createMediaStream = async () => {
-      const stream: MediaStream = await navigator.mediaDevices.getUserMedia({
-        video: {
-          width: { min: 640, ideal: 1920 },
-          height: { min: 400, ideal: 1080 },
-          aspectRatio: { ideal: 1.7777777778 },
-        },
+      const stream: MediaStream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
         audio: true,
       });
 
