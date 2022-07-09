c5f3af32623975f1b9e9891bc4ddf9cd08bfaa65
071yoon <yeonggilol@gmail.com>
2022-07-09 14:22:55 +0900

    FIRE-282 add global font
    globalfont 적용
    figma 기준 헤더인 gmarket font 추가

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,27 @@
 import { createGlobalStyle } from 'styled-components';
+import GmarketSansTTFBold from '../fonts/GmarketSansTTFBold.ttf';
+import GmarketSansTTFMedium from '../fonts/GmarketSansTTFMedium.ttf';
+import GmarketSansTTFLight from '../fonts/GmarketSansTTFLight.ttf';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+        font-family: 'GmarketSansTTFBold';
+        src: local('GmarketSansTTFBold'), local('GmarketSansTTFBold');
+        font-style: normal;
+        src: url(${GmarketSansTTFBold}) format('truetype');
+  }
+  @font-face {
+        font-family: 'GmarketSansTTFMedium';
+        src: local('GmarketSansTTFMedium'), local('GmarketSansTTFMedium');
+        font-style: normal;
+        src: url(${GmarketSansTTFMedium}) format('truetype');
+  }
+  @font-face {
+        font-family: 'GmarketSansTTFLight';
+        src: local('GmarketSansTTFLight'), local('GmarketSansTTFLight');
+        font-style: normal;
+        src: url(${GmarketSansTTFLight}) format('truetype');
+  }
 html,
 body {
   font-size: 62.5%;
