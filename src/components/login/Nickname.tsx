21cb69c6a35cc03a3591694c586ecb35ac4387bd
leeharyung <lhr4884@naver.com>
2022-07-12 14:36:06 +0900

    FIRE-307 fix modal ui

---
+++ src/components/login/Nickname.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import UserStore from '../../stores/userStore';
+
+export default function Nickname() {
+  const { nickname } = UserStore();
+  const [newNickname, setNewNickname] = useState(nickname);
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewNickname(event.target.value);
+  };
+
+  return (
+    <Component>
+      <Input
+        autoComplete="off"
+        value={newNickname}
+        onChange={onChange}
+        placeholder="닉네임을 입력해주세요"
+        id="nickname"
+      />
+      <Button>확인</Button>
+    </Component>
+  );
+}
+
+const Component = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  height: 12rem;
+  width: 25rem;
+`;
+
+const Input = styled.input`
+  background-color: rgb(30, 41, 75);
+  color: white;
+  font-size: 1.7rem;
+  text-justify: center;
+  padding-left: 1rem;
+  width: 100%;
+`;
+
+const Button = styled.button`
+  font-family: PretendardRegular;
+  font-weight: 600;
+  height: 6rem;
+  width: 100%;
+  border-radius: 0.8rem;
+  font-size: 1.5rem;
+  background-color: white;
+  margin-top: 2rem;
+  cursor: pointer;
+`;
