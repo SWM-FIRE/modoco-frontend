9efae9e7c980071c11c88cc795facd5ce23b78dd
YeongGi Yoon <yeonggi@kakao.com>
2022-07-13 17:29:07 +0900

    FIRE-232 block react-query
    block을 react-query 에서 가져올 수 있게 설정

--- src/components/main/Scrolls.tsx
+++ src/components/main/Scrolls.tsx
@@ -1,44 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ScrollMenu } from 'react-horizontal-scrolling-menu';
+import { useQuery } from 'react-query';
+import axios from 'axios';
 import { LeftArrow, RightArrow } from './Arrow';
 import Block from './Block';
 import blockInterface from '../../interface/block.interface';
-import blocksData from '../../blocks.json';
 import TagStore from '../../stores/tagStore';
 
 export default function Scrolls() {
   const { tag } = TagStore();
-  const filteredData = blocksData.filter((block) =>
-    block.tags.some((blockTag) =>
-      blockTag.toLowerCase().includes(tag.toLowerCase()),
-    )
-      ? block
-      : null,
-  );
+
+  const { isLoading, error, data } = useQuery('roomData', async () => {
+    const { data } = await axios.get('https://xn--hq1br4kwqt.com/api/v1/rooms');
+    return data;
+  });
+
+  if (isLoading)
+    return <div style={{ color: 'white', fontSize: '10rem' }}>loading....</div>;
+
+  if (error) return <div>An error has occurred: </div>;
+
   return (
     <Container>
       <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-        {filteredData.map(
-          ({ moderator, title, detail, tags, itemId }: blockInterface) => {
-            return (
-              <Block
-                itemId={itemId}
-                key={moderator.nickname}
-                moderator={moderator}
-                title={title}
-                detail={detail}
-                tags={tags}
-                current={0}
-                total={0}
-              />
-            );
-          },
-        )}
+        {data
+          .filter((block) =>
+            block.tags.some((blockTag) =>
+              blockTag.toLowerCase().includes(tag.toLowerCase()),
+            )
+              ? block
+              : null,
+          )
+          .map(
+            ({ moderator, title, details, tags, itemId }: blockInterface) => {
+              return (
+                <Block
+                  itemId={itemId}
+                  key={moderator.nickname}
+                  moderator={moderator}
+                  title={title}
+                  details={details}
+                  tags={tags}
+                  current={0}
+                  total={0}
+                />
+              );
+            },
+          )}
       </ScrollMenu>
     </Container>
   );
 }
+
 const Container = styled.div`
   margin-top: 3.6rem;
   width: calc(100% - 10rem);
