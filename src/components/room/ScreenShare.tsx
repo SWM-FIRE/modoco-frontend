312672ba217fcd778194ea515fc2d922bd0fa8e8
YeongGi Yoon <yeonggi@kakao.com>
2022-07-18 22:51:48 +0900

    FIRE-327 set avatar at screen share
    screenshare media query 및 아바타 배치 완료

--- src/components/room/ScreenShare.tsx
+++ src/components/room/ScreenShare.tsx
@@ -1,29 +1,67 @@
 import styled from 'styled-components';
+import MyAvatar from '../../assets/avatar/MyAvatar';
 
 export default function ScreenShare() {
   return (
     <Container>
       <ScreenWrapper>
-        <Screen />
-        <Screen />
-        <Screen />
-        <Screen />
+        <Screen>
+          <AvatarPosition>
+            <MyAvatar num={1} />
+          </AvatarPosition>
+        </Screen>
+        <Screen>
+          <AvatarPosition>
+            <MyAvatar num={5} />
+          </AvatarPosition>
+        </Screen>
+        <Screen>
+          <AvatarPosition>
+            <MyAvatar num={10} />
+          </AvatarPosition>
+        </Screen>
+        <Screen>
+          <AvatarPosition>
+            <MyAvatar num={15} />
+          </AvatarPosition>
+        </Screen>
       </ScreenWrapper>
     </Container>
   );
 }
 
+const AvatarPosition = styled.div`
+  bottom: -8%;
+  width: 20%;
+  height: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    width: 100%;
+  }
+  position: absolute;
+`;
+
 const Screen = styled.div`
   background-color: white;
-  @media (max-width: 768px) {
-    width: 80%;
+  display: flex;
+  justify-content: center;
+  @media (max-width: 900px) {
+    width: 60%;
     height: 0;
-    padding-bottom: 50%;
+    padding-bottom: 38%;
+  }
+  @media (max-width: 768px) {
+    width: 75%;
+    height: 0;
+    padding-bottom: 45%;
   }
   width: 45%;
   height: 0;
   padding-bottom: 25%;
   border: 1px solid black;
+  position: relative;
 `;
 
 const ScreenWrapper = styled.div`
@@ -32,17 +70,18 @@ const ScreenWrapper = styled.div`
   align-items: center;
   flex-wrap: wrap;
   overflow: auto;
-  @media (max-width: 768px) {
+  @media (max-width: 900px) {
     flex-direction: column;
     flex-wrap: unset;
+    gap: 5%;
   }
-  gap: 5%;
+  gap: 0 3%;
   height: 100%;
 `;
 
 const Container = styled.div`
-  margin: 5.6rem 0 2.5rem 2.5rem;
+  margin: 5.6rem 0 0 2.5rem;
   width: calc(100% - 46.5rem);
-  height: calc(100% - 10rem);
+  height: calc(100% - 7rem);
   background-color: gray;
 `;
