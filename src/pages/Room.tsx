0ce444c4e06494b8d3b7d7c5a8c0eb05441de832
leeharyung <lhr4884@naver.com>
2022-07-19 13:59:14 +0900

    FIRE-329 fix room.tsx

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,22 +1,19 @@
 import styled from 'styled-components';
 import { useEffect } from 'react';
-import { io } from 'socket.io-client';
 import Header from '../components/room/Header';
 import ScreenShare from '../components/room/ScreenShare';
 import Sidebar from '../components/room/Sidebar';
+import { initSocket, disconnectSocket } from '../adaptors/chat/socketio';
 
 export default function Room() {
   useEffect(() => {
     // socket connection
     console.log('connecting...');
-    const socket = io('http://localhost:3333/socket/chat');
-    socket.on('connect', () => {
-      console.log('socket connected');
-    });
+    initSocket();
 
     return () => {
       // socket disconnection
-      socket.disconnect();
+      disconnectSocket();
     };
   }, []);
   return (
