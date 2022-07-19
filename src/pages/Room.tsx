feb9c9d6c55ea34f0b090aae694acfc14a519d44
leeharyung <lhr4884@naver.com>
2022-07-19 13:53:48 +0900

    FIRE-329 create adaptors/chat folder

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,9 +1,24 @@
 import styled from 'styled-components';
+import { useEffect } from 'react';
+import { io } from 'socket.io-client';
 import Header from '../components/room/Header';
 import ScreenShare from '../components/room/ScreenShare';
 import Sidebar from '../components/room/Sidebar';
 
 export default function Room() {
+  useEffect(() => {
+    // socket connection
+    console.log('connecting...');
+    const socket = io('http://localhost:3333/socket/chat');
+    socket.on('connect', () => {
+      console.log('socket connected');
+    });
+
+    return () => {
+      // socket disconnection
+      socket.disconnect();
+    };
+  }, []);
   return (
     <Component>
       <Header />
