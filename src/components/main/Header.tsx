9050e513cbe5d3237bc1c2f4fc4f9490708641b5
leeharyung <lhr4884@naver.com>
2022-08-03 15:49:33 +0900

    FIRE-392 add test for lottie

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -10,6 +10,7 @@ export default function Header({ modalHandler }: { modalHandler: () => void }) {
   const clickLogo = () => {
     navigate('/#/');
   };
+
   return (
     <Container>
       <Logo onClick={clickLogo}>modoco</Logo>
