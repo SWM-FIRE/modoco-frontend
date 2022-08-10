715a7cdaa11a813b847bcb609a526510c322d117
leeharyung <lhr4884@naver.com>
2022-08-10 22:06:01 +0900

    FIRE-435 edit user information

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { API } from '../config';
+import userStore from '../stores/userStore';
 
 export default function useLogin() {
   const navigate = useNavigate();
+  const { setNickname, setAvatar, setToken } = userStore();
   const [inputs, setInputs] = useState({
     email: '',
     password: '',
@@ -30,12 +32,26 @@ export default function useLogin() {
         password,
       })
       .then((res) => {
-        console.log('[success]', res.data.access_token);
+        console.log('[success]', res.data);
         localStorage.setItem('access_token', res.data.access_token);
-        navigate(`/main`);
+        console.log('[token]', localStorage.getItem('access_token'));
+        setToken(res.data.access_token);
+        axios
+          .get(API.ME, {
+            headers: {
+              Authorization: `Bearer ${res.data.access_token}`,
+            },
+          })
+          .then((res) => {
+            console.log('[me]', res.data);
+            setNickname(res.data.nickname);
+            setAvatar(res.data.avatar);
+            navigate(`/main`);
+          });
       })
       .catch((err) => {
         console.log('[error] ', err);
+        console.log('[error messgae]', err.response.data.message);
       });
   };
 
