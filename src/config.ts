da094e4d22750f8d120a315e87565d94f7f1145e
leeharyung <lhr4884@naver.com>
2022-08-10 18:45:04 +0900

    FIRE-435 add session in config

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,12 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 const API_VER = '/api/v1';
 const USER = '/users/';
 const ROOM = '/rooms/';
+const SESSION = './session/';
 const SOCKET = '/socket/room';
 
 export const API = {
   SOCKET: `${BASE_URL}${SOCKET}`,
   ROOM: `${BASE_URL}${API_VER}${ROOM}`,
   USER: `${BASE_URL}${API_VER}${USER}`,
+  SESSION: `${BASE_URL}${API_VER}${SESSION}`,
 };
