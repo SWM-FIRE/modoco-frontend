faed48779a5e13776b8038a06ef58762e3fbcd5b
leeharyung <lhr4884@naver.com>
2022-08-15 20:23:26 +0900

    edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import {
-  unstable_HistoryRouter as HistoryRouter,
-  Route,
-  Routes,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { createBrowserHistory } from 'history';
-import jwtDecode from 'jwt-decode';
 import './fonts/font.css';
 import Layout from './components/layout/Layout';
 import Room from './pages/Room';
@@ -21,22 +15,9 @@ import SignUp from './pages/SignUp';
 const queryClient = new QueryClient();
 
 function App() {
-  const history = createBrowserHistory();
-
-  history.listen(() => {
-    const user = localStorage.getItem('access_token');
-    if (user) {
-      const decodedJwt = jwtDecode(user);
-      console.log('[decodedJwt]', decodedJwt);
-      // if (decodedJwt.exp * 1000 < Date.now()) {
-      //   localStorage.removeItem('access_token');
-      //   alert('로그인 시간이 만료되었습니다.');
-      // }
-    }
-  });
   return (
     <QueryClientProvider client={queryClient}>
-      <HistoryRouter history={history}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<LandingPage />} />
@@ -59,7 +40,7 @@ function App() {
           <Route path="/test" element={<Test />} />
           <Route path="*" element={<Error />} />
         </Routes>
-      </HistoryRouter>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
