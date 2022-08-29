387566c21ee96f3a808dfecc17a2c8a22bf883bd
leeharyung <lhr4884@naver.com>
2022-08-30 01:01:21 +0900

    FIRE-517 edit receiveNewMessageStore

--- src/stores/receiveNewMessageStore.ts
+++ src/stores/receiveNewMessageStore.ts
@@ -2,12 +2,17 @@ import create from 'zustand';
 
 interface receiveNew {
   isReceiveNewMessage: boolean;
-  setIsReceiveNewMessage: (_state: boolean) => void;
+  isAlarmToggle: boolean;
+  setIsReceiveNewMessage: (_by: boolean) => void;
+  setIsAlarmToggle: () => void;
 }
 const receiveNewMessageStore = create<receiveNew>((set) => ({
   isReceiveNewMessage: false,
+  isAlarmToggle: false,
   setIsReceiveNewMessage: (state: boolean) =>
     set(() => ({ isReceiveNewMessage: state })),
+  setIsAlarmToggle: () =>
+    set((state) => ({ isAlarmToggle: !state.isAlarmToggle })),
 }));
 
 export default receiveNewMessageStore;
