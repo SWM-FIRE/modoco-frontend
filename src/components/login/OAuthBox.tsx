3c452e3c155599073cd2e885276b01f3588495fd
YeongGi Yoon <yeonggi@kakao.com>
2022-09-05 20:17:00 +0900

    FIRE-553 send OAuth client-id and get auth code

--- src/components/login/OAuthBox.tsx
+++ src/components/login/OAuthBox.tsx
@@ -5,8 +5,17 @@ import Github from '../../assets/img/oauth/github.png';
 import Google from '../../assets/img/oauth/google.png';
 
 export default function OAuthBox({ auth }: { auth: string }) {
+  const GithubURI = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}`;
+  const KakaoURI = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
+  const GoogleURI = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email`;
+
+  const handleLogin = () => {
+    window.location.href =
+      auth === 'kakao' ? KakaoURI : auth === 'github' ? GithubURI : GoogleURI;
+  };
+
   return (
-    <Button>
+    <Button onClick={handleLogin}>
       <Img
         src={auth === 'kakao' ? Kakao : auth === 'github' ? Github : Google}
         alt={auth}
