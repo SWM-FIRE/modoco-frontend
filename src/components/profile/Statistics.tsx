ce7740f1c4d932ad78634ffb96d1e9e4824b2251
leeharyung <lhr4884@naver.com>
2022-09-08 01:48:32 +0900

    FIRE-575 add Chart.jsx

--- src/components/profile/Statistics.tsx
+++ src/components/profile/Statistics.tsx
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import Chart from './Chart/Chart';
 
 export default function Statistics({ isMe }) {
+  const [selectedType, setSelectedType] = useState('Day');
+  const onClickTab = (e) => {
+    setSelectedType(e.target.innerText);
+  };
   return (
     <Components>
       <Title>코딩 추이</Title>
-      <Ready isMe={isMe}>준비중입니다</Ready>
+      <Contents isMe={isMe}>
+        <Tabs>
+          <Type onClick={onClickTab} isOnClicked={selectedType === 'Day'}>
+            Day
+          </Type>
+          <Type onClick={onClickTab} isOnClicked={selectedType === 'Week'}>
+            Week
+          </Type>
+          <Type onClick={onClickTab} isOnClicked={selectedType === 'Month'}>
+            Month
+          </Type>
+        </Tabs>
+        <Chart type={selectedType} />
+      </Contents>
     </Components>
   );
 }
@@ -18,19 +37,29 @@ const Title = styled.h1`
   font-size: 4rem;
 `;
 
-const Ready = styled.div<{ isMe: boolean }>`
+const Contents = styled.div<{ isMe: boolean }>`
   width: ${(props) => (props.isMe ? '70%' : '100%')};
-  height: 43.6rem;
-  padding: 3.2rem;
-  background-color: #6e768f5e;
   border-radius: 2rem;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
   font-size: 5rem;
   color: #f9fafb56;
-  margin-top: 1.4rem;
+  gap: 3rem;
   @media (max-width: 1020px) {
     width: 100%;
   }
+`;
+
+const Tabs = styled.div``;
+
+const Type = styled.button<{ isOnClicked: boolean }>`
+  color: ${(props) =>
+    props.isOnClicked ? '#ffffff' : 'rgba(255, 255, 255, 0.5)'};
+  padding: 1rem 1.75rem;
+  background-color: #000000;
+  font-size: 1.4rem;
+  cursor: pointer;
+  font-weight: 700;
 `;
