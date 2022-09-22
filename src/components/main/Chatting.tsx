869fec615ba180e1e3b34bda7cd4641f5d48a23a
071yoon <yeonggilol@gmail.com>
2022-09-22 18:01:51 +0900

    FIRE-653 add lobby chatting

---
+++ src/components/main/Chatting.tsx
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+import React, { useCallback, useRef, useEffect } from 'react';
+import ChattingItem from '../room/sideBar/ChattingItem';
+import lobbyMessageStore from '../../stores/lobbyMessageStore';
+import userStore from '../../stores/userStore';
+import SendChat from '../atoms/chatting/SendChat';
+
+export default function Chat() {
+  const chatWindow = useRef(null);
+  const { messages } = lobbyMessageStore();
+  const { uid } = userStore();
+
+  useEffect(() => {
+    moveScrollToReceiveMessage('auto', true);
+  }, []);
+
+  useEffect(() => {
+    if (messages[messages.length - 1]?.uid === uid)
+      moveScrollToReceiveMessage('smooth', true);
+    else moveScrollToReceiveMessage('smooth', false);
+  }, [messages]);
+
+  const moveScrollToReceiveMessage = useCallback(
+    (behavior: string, isFirstView: boolean) => {
+      if (chatWindow.current) {
+        const isScroll =
+          chatWindow.current.scrollHeight - chatWindow.current.scrollTop <=
+            chatWindow.current.clientHeight + 100 || isFirstView;
+        if (isScroll) {
+          chatWindow.current.scrollTo({
+            top: chatWindow.current.scrollHeight,
+            behavior,
+          });
+        }
+      }
+    },
+    [],
+  );
+
+  return (
+    <Component>
+      <ChattingList ref={chatWindow}>
+        {messages.map((message) => (
+          <ChattingItem
+            key={message.uid + message.createdAt}
+            user={{
+              nickname: message.nickname,
+              avatar: message.avatar,
+              uid: message.uid,
+            }}
+            msg={message.message}
+            time={message.createdAt}
+            type={message.type}
+            isHideTime={message.isHideTime}
+            isHideNicknameAndAvatar={message.isHideNicknameAndAvatar}
+          />
+        ))}
+      </ChattingList>
+      <SendChat roomId="lobby" uid={uid} />
+    </Component>
+  );
+}
+
+export const Chatting = React.memo(Chat);
+
+const Component = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  height: calc(100% - 5.8rem);
+  font-family: IBMPlexSansKRRegular;
+  font-size: 1.3rem;
+  color: #9ca3af;
+`;
+
+const ChattingList = styled.ul`
+  flex-grow: 1;
+  width: 100%;
+  overflow: auto;
+  height: 100%;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
