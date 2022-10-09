5483a8ffa0b6516945e15a006de50866b4b1952b
leeharyung <lhr4884@naver.com>
2022-10-10 04:34:42 +0900

    FIRE-731 fix react-query in edit user

--- src/hooks/useChangeProfile.ts
+++ src/hooks/useChangeProfile.ts
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { useMutation, useQueryClient } from 'react-query';
 import toast from 'react-hot-toast';
 import userStore from '../stores/userStore';
 import { changeMe } from '../api/main';
 
 export default function useChangeProfile() {
   const { setAvatar, setNickname, setDescription, uid } = userStore();
+  const queryClient = useQueryClient();
   const {
     avatar: myAvatar,
     nickname: myNickname,
@@ -36,22 +38,28 @@ export default function useChangeProfile() {
     });
   };
 
+  const { mutate } = useMutation(() => changeMe(uid, nickname, avatar), {
+    onSuccess: (res) => {
+      console.debug('[success]', res);
+      setAvatar(avatar);
+      setNickname(nickname);
+      setDescription(description);
+      toast.success('유저 정보가 변경되었습니다');
+    },
+    onError: (err) => {
+      console.debug('[error] ', err);
+      setInputs({
+        ...inputs,
+      });
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['userData', 'getOne', uid]);
+    },
+  });
+
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    changeMe(uid, nickname, avatar)
-      .then((res) => {
-        console.debug('[success]', res);
-        setAvatar(avatar);
-        setNickname(nickname);
-        setDescription(description);
-        toast.success('유저 정보가 변경되었습니다');
-      })
-      .catch((err) => {
-        console.debug('[error] ', err);
-        setInputs({
-          ...inputs,
-        });
-      });
+    mutate();
   };
 
   return {
