5103390dec21116ee526fc1bf3515e569d217e0a
leeharyung <lhr4884@naver.com>
2022-10-23 16:21:36 +0900

    FIRE-768 add thumbnails img

--- src/api/core/index.ts
+++ src/api/core/index.ts
@@ -1,3 +1,3 @@
 export { awsRequest } from './awsLambdaAxios';
 export { authorizationRequest, unAuthorizationRequest } from './backendAxios';
-export { youtubeAxios, thumbnailAxios } from './youtubeAxios';
+export { youtubeAxios } from './youtubeAxios';
