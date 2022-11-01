59d0ef60dba0bb3d137e64eebf79b24b45aaf25b
YeongGi Yoon <yeonggi@kakao.com>
2022-11-01 17:15:33 +0900

    FIRE-800 improve friend direct message algorithm

--- src/components/atoms/chattingModal/ChattingModal.tsx
+++ src/components/atoms/chattingModal/ChattingModal.tsx
@@ -4,7 +4,6 @@ import useUser from 'src/hooks/useUser';
 import MyAvatar from 'src/assets/avatar/MyAvatar';
 import userStore from 'src/stores/userStore';
 import directMessageStore from 'src/stores/directMessageStore';
-import { newMessageInterface } from 'src/interface/directMessage.interface';
 import ChattingPortal from './ChattingPortal';
 import SendChat from './SendChat';
 import SingleDirectChat from './SingleDirectChat';
@@ -15,46 +14,6 @@ export default function ChattingModal() {
   const { isLoading, error, data } = useUser(Number(chattingFriend));
   const { messages } = directMessageStore();
   const { uid } = userStore();
-
-  const filteredMessage = messages[chattingFriend]
-    ? messages[chattingFriend].sort((a, b) => {
-        return Number(a.createdAt) - Number(b.createdAt);
-      })
-    : [];
-
-  // 채팅을 보낼 때 같은 군집으로 보낼지 여부를 결정
-  // 앞의 message와 비교해서 내가 저번에 보냈고, 같은 분에 속해있다면 true 아니면 false
-  const newMessageType: newMessageInterface[] = [];
-  newMessageType.push({
-    ...filteredMessage[0],
-    isHide: false,
-    hideTime: false,
-  });
-  for (let i = 1; i < filteredMessage.length; i += 1) {
-    const prev = filteredMessage[i - 1];
-    const cur = filteredMessage[i];
-
-    const prevDay = new Date(Number(prev.createdAt)).getDate();
-    const curDay = new Date(Number(cur.createdAt)).getDate();
-    const prevHour = new Date(Number(prev.createdAt)).getHours();
-    const curHour = new Date(Number(cur.createdAt)).getHours();
-    const prevMin = new Date(Number(prev.createdAt)).getMinutes();
-    const curMin = new Date(Number(cur.createdAt)).getMinutes();
-
-    if (
-      cur.from === prev.from &&
-      cur.to === prev.to &&
-      curDay === prevDay &&
-      curHour === prevHour &&
-      curMin === prevMin
-    ) {
-      newMessageType.push({ ...cur, isHide: true, hideTime: false });
-      newMessageType[i - 1].hideTime = true;
-    } else {
-      newMessageType.push({ ...cur, isHide: false, hideTime: false });
-    }
-  }
-  newMessageType.reverse();
 
   if (isLoading)
     return (
@@ -95,16 +54,18 @@ export default function ChattingModal() {
             </Avatar>
           </Header>
           <Content>
-            {newMessageType.map((singleChat, index) => {
-              return (
-                <SingleDirectChat
-                  key={Symbol(index).toString()}
-                  singleChat={singleChat}
-                  target={data}
-                  me={uid}
-                />
-              );
-            })}
+            {Array.isArray(messages[data.uid]) && messages[data.uid].length > 0
+              ? messages[data.uid].map((singleChat, index) => {
+                  return (
+                    <SingleDirectChat
+                      key={Symbol(index).toString()}
+                      singleChat={singleChat}
+                      target={data}
+                      me={uid}
+                    />
+                  );
+                })
+              : null}
           </Content>
           <Footer>
             <SendChat uid={data?.uid} />
