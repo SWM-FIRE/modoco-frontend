d7717dd54be713b4c6c0eb7ecf3f7ee9424b1341
YeongGi <yeonggilol@gmail.com>
2023-08-23 21:39:57 +0900

    feat: 방을 itemID 대신 createdAt으로 정렬하도록 수정

--- src/interface/block.interface.ts
+++ src/interface/block.interface.ts
@@ -9,6 +9,8 @@ export default interface blockInterface {
   tags: string[];
   current: number;
   total: number;
+  // string으로 받지만 사용시에 parse하여 사용
+  createdAt: string;
   theme: string;
   isPublic: boolean;
 }
